6088219a4e795a9dd31e8f373c166a6e8e801f15
Hema Gupta <hemafun90@gmail.com>
2020-09-15 17:58:41 +0530

    Added code

---
+++ Day1/session1/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+
+      home: textDemo(),
+    );
+  }
+}
+
+class textDemo extends StatefulWidget {
+  @override
+  _textDemoState createState() => _textDemoState();
+}
+
+class _textDemoState extends State<textDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Session 1"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hema")
+          ],
+        ),
+      ),
+    );
+  }
+}
